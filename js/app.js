45f8772dbf23d99a36dcfbe838ee0b14eefc9766
Jhosse Paul Márquez Ruíz <mcromanceva@hotmail.com>
2016-04-28 10:45:10 -0500

    Update 2016-04-28T15:44:52.035Z

--- js/app.js
+++ js/app.js
@@ -11,22 +11,47 @@
     }
   });
 
-  new WOW().init();
+  width = 600;
 
-  width = 960;
-
-  height = 500;
+  height = 300;
 
   color = d3.scale.category20();
 
   force = d3.layout.force().charge(-120).linkDistance(30).size([width, height]);
 
-  svg = d3.select("body").append("svg").attr("width", width).attr("height", height);
+  svg = d3.select("#network").append("svg").attr("width", width).attr("height", height);
 
-  d3.json("miserables.json", function(error, graph) {
+  d3.json("/assets/data/network.json", function(error, graph) {
+    var link, node;
     if (error) {
       throw error;
     }
+    force.nodes(graph.nodes).links(graph.links).start();
+    link = svg.selectAll(".link").data(graph.links).enter().append("line").attr("class", "link").style("stroke-width", function(d) {
+      return Math.sqrt(d.value);
+    });
+    node = svg.selectAll(".node").data(graph.nodes).enter().append("circle").attr("class", "node").attr("r", 5).style("fill", function(d) {
+      return color(d.group);
+    }).call(force.drag);
+    node.append("title").text(function(d) {
+      return color(d.group);
+    });
+    force.on("tick", function() {
+      link.attr("x1", function(d) {
+        return d.source.x;
+      }).attr("y1", function(d) {
+        return d.source.y;
+      }).attr("x2", function(d) {
+        return d.target.x;
+      }).attr("y2", function(d) {
+        return d.target.y;
+      });
+      node.attr("cx", function(d) {
+        return d.x;
+      }).attr("cy", function(d) {
+        return d.y;
+      });
+    });
   });
 
 }).call(this);
